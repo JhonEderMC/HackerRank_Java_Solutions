e543b6b022c7e144f14ae7d98436c533351d7128
jhonedermc <jhoneder@hotmail.com>
2025-01-09 22:09:26 -0500

    Introduction -> Java String Tokens solution

---
+++ src/introduction/Java_String_Tokens_19/Solution.java
@@ -0,0 +1,115 @@
+package introduction.Java_String_Tokens_19;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/*
+Given a string, , matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.
+
+Note: You may find the String.split method helpful in completing this challenge.
+
+Input Format
+
+A single string, .
+
+Constraints
+
+ is composed of any of the following: English alphabetic letters, blank spaces, exclamation points (!), commas (,), question marks (?), periods (.), underscores (_), apostrophes ('), and at symbols (@).
+Output Format
+
+On the first line, print an integer, , denoting the number of tokens in string  (they do not need to be unique). Next, print each of the  tokens on a new line in the same order as they appear in input string .
+
+Sample Input
+
+He is a very very good boy, isn't he?
+Sample Output
+
+10
+He
+is
+a
+very
+very
+good
+boy
+isn
+t
+he
+Explanation
+
+We consider a token to be a contiguous segment of alphabetic characters. There are a total of  such tokens in string , and each token is printed in the same order in which it appears in string .
+
+Language
+Java 15
+More
+567891112141315421617311018
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        scanner.close();
+
+Line: 10 Col: 25
+
+Test against custom input
+Congratulations
+You solved this challenge. Would you like to challenge your friends?Share on FacebookShare on TwitterShare on LinkedIn
+
+Test case 0
+
+Test case 1
+
+Test case 2
+
+Test case 3
+
+Test case 4
+
+Test case 5
+
+Test case 6
+
+Test case 7
+
+Test case 8
+
+Test case 9
+
+Test case 10
+Compiler Message
+Success
+Input (stdin)
+He is a very very good boy, isn't he?
+Expected Output
+10
+He
+is
+a
+very
+very
+good
+boy
+isn
+t
+he
+
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+
+        List<String> list = List.of(string.split("[^a-zA-Z]"));
+        list = list.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
+        System.out.println(list.size());
+        list.forEach(System.out::println);
+    }
+}
