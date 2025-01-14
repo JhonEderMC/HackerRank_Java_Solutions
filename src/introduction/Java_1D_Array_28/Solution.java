e7943c11b4b257f5c40cbc668ef65744d5c9f0f3
jhonedermc <jhoneder@hotmail.com>
2025-01-14 16:43:11 -0500

    Introduction -> Java 1D Array -> Solution

---
+++ src/introduction/Java_1D_Array_28/Solution.java
@@ -0,0 +1,20 @@
+package introduction.Java_1D_Array_28;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        List<Integer> list = Stream.generate(scanner::nextInt).limit(n).collect(Collectors.toList());
+
+        list.forEach(System.out::println);
+    }
+
+
+}
