b9d6f30c476cd7c460cb7dc83f2afba35f518ba9
jhonedermc <jhoneder@hotmail.com>
2025-02-05 22:29:41 -0500

    Data Structures -> Java Generics -> Solution

---
+++ src/Data_Structures/Java_Generics_10/Solution.java
@@ -0,0 +1,23 @@
+package Data_Structures.Java_Generics_10;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+    public static  void main(String [] args) {
+        ArrayList list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add("Hello");
+        list.add("World");
+
+        printArray(list.toArray());
+    }
+
+   public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+   }
+}
